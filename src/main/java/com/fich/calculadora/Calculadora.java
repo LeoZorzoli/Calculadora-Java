7c7264afa79b86f1acd32519fd71b3846084fa83
Leonel Zorzoli <63476706+LeoZorzoli@users.noreply.github.com>
2023-06-05 12:43:09 -0300

    Add files via upload

---
+++ src/main/java/com/fich/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.fich.calculadora;
+
+/**
+ *
+ * @author Leonel Zorzoli
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        VentanaPrincipal panel = new VentanaPrincipal();
+        panel.setVisible(true);
+    }
+}
